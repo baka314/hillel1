c30855695310fd4310b6af63788500f5820a3421
suprastinka <zelinska.1221204@stud.op.edu.ua>
2022-08-02 12:54:04 +0300

    baka's commit

---
+++ src/hillelJavaPro/Main.java
@@ -0,0 +1,10 @@
+package hillelJavaPro;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello EugeniyaDzhu");
+        System.out.println("Hello Java Pro");
+        System.out.println("Hello Java Pro");
+        System.out.println("Hello everybody! Slava Ukraine");
+    }
+}
